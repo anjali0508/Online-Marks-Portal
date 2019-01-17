bc8a7e0c903a3b385446eca34a5a1e2b6c321e68
anjali0508 <46600043+anjali0508@users.noreply.github.com>
2019-01-17 16:10:53 +0530

    Update databaseconn.php

--- databaseconn.php
+++ databaseconn.php
@@ -1,7 +1,7 @@
 <?php
 $dbServername="localhost";
 $dbUsername="root";
-$dbPassword="";
+$dbPassword="anjali";
 $dbName="wp project";
 $conn=mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
 if (mysqli_connect_errno())
